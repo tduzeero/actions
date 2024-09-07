226f69e317a4c52346ed400f08535744fc353e47
wxyzeero <wxyzeero@gmail.com>
2024-09-06 21:50:09 -0300

    fix: chmod permissions .sh files

--- publish.sh
+++ publish.sh
@@ -7,6 +7,12 @@ git push origin :v${VERSION}
 git tag -d v${VERSION}
 
 git add .
+git update-index --chmod=+x tag/commit/get-commit-list.sh
+git update-index --chmod=+x tag/create/create-tag.sh
+git update-index --chmod=+x user/config/set-config-user.sh
+git update-index --chmod=+x version/computed/get-computed-version.sh
+git update-index --chmod=+x version/current/get-current-version.sh
+git update-index --chmod=+x version/update/update-version.sh
 git commit -m "$MESSAGE"
 git push
 
