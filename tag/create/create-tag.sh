cfa5f609fcab21077f7236463b705ee26416fdc3
wxyzeero <wxyzeero@gmail.com>
2024-09-06 23:16:42 -0300

    fix: chmod permissions .sh files

--- tag/create/create-tag.sh
+++ tag/create/create-tag.sh
@@ -7,5 +7,8 @@ MESSAGE=$(prinf $2 "${CURRENT_TAG}" "$COMMIT_LIST")
 echo "Tag version: ${CURRENT_TAG}"
 echo "Commits from last tag: ${COMMIT_LIST}"
 
+
+exit 1
+
 git tag -a "${CURRENT_TAG}" -m "${MESSAGE}"
 git push origin tag "${CURRENT_TAG}"
--- user/config/set-config-user.sh
+++ user/config/set-config-user.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
-USER_TYPE=$1
+USER_FROM=$1
 USER_NAME="github-actions[bot]"
 USER_EMAIL="41898282+github-actions[bot]@users.noreply.github.com"
 
-if [[ "$USER_TYPE" == "commit-head" ]]; then
+echo "USER_FROM=${USER_FROM}"
+
+if [[ "$USER_FROM" == "commit-head" ]]; then
   USER_NAME=$(git show -s --format='%an')
   USER_EMAIL=$(git show -s --format='%ae')
 fi
+
+echo "USER_NAME=${USER_NAME}"
+echo "USER_EMAIL=${USER_EMAIL}"
 
 git config user.name $USER_NAME
 git config user.email $USER_EMAIL
