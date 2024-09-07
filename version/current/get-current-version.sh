2c09c7d6b3b73164bcff74def2b4093886f56c86
wxyzeero <wxyzeero@gmail.com>
2024-09-07 12:25:32 -0300

    fix: chmod permissions .sh files

--- version/current/get-current-version.sh
+++ version/current/get-current-version.sh
@@ -5,7 +5,7 @@ CURRENT_VERSION=$2
 MATCH_PATTERN="v+([0-9]|!(*[a-z]*)[0-9]).+([0-9]|!(*[a-z]*)[0-9]).+([0-9]|!(*[a-z]*)[0-9])"
 
 if [[ $VERSION_FROM == "git" ]]; then
-  CURRENT_TAG=$(git describe --tags --abbrev=0 --match="${MATCH_PATTERN}")
+  CURRENT_TAG=$(git describe --tags --abbrev=0 --always --match="${MATCH_PATTERN}")
   echo "CURRENT_TAG: ${CURRENT_TAG}" >&2
   if [[ $CURRENT_TAG != "" ]]; then
     CURRENT_VERSION=$CURRENT_TAG
