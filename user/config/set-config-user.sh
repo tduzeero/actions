3621b5e87d03496679cf57924f6dfa23663fc753
wxyzeero <wxyzeero@gmail.com>
2024-09-09 19:38:33 -0300

    fix: chmod permissions .sh files

--- user/config/set-config-user.sh
+++ user/config/set-config-user.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 
 USER_FROM=$1
-APP_SLUG=$2
-USER_ID=$3
-
-USER_NAME="github-actions[bot]"
-USER_EMAIL="41898282+github-actions[bot]@users.noreply.github.com"
-
-if [[ "$USER_FROM" == "app-bot" ]]; then
-  USER_NAME="${APP_SLUG}[bot]"
-  USER_EMAIL="${USER_ID}+${APP_SLUG}[bot]@users.noreply.github.com"
-fi
+USER_NAME=$2
+USER_EMAIL=$3
 
 if [[ "$USER_FROM" == "commit-head" ]]; then
   USER_NAME=$(git show -s --format='%an')
@@ -21,5 +13,5 @@ echo "USER_FROM: ${USER_FROM}" >&2
 echo "USER_NAME: ${USER_NAME}" >&2
 echo "USER_EMAIL: ${USER_EMAIL}" >&2
 
-git config user.name $USER_NAME
-git config user.email $USER_EMAIL
+git config user.name "$USER_NAME"
+git config user.email "$USER_EMAIL"
