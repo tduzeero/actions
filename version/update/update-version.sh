fd455aa86245123f5dc04ae2ee51d83ff8b6c044
wxyzeero <wxyzeero@gmail.com>
2024-10-10 19:13:56 -0300

    fix: at least +1 on empty commit list

--- version/update/update-version.sh
+++ version/update/update-version.sh
@@ -7,7 +7,7 @@ echo "COMMIT_MESSAGE: ${COMMIT_MESSAGE}" >&2
 
 git pull
 
-sed -i '/"version": *"[0-9].[0-9].[0-9]"/ s/"[0-9].[0-9].[0-9]"/"'$NEW_VERSION'"/' package.json
+sed -ie 's/"version": .*"/"version": '\"${$NEW_VERSION}\"'/g' package.json
 
 git add .
 git commit -m "$COMMIT_MESSAGE"
