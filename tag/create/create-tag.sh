2da950fbfa3a3ce8f520d8932d2d19447b8cfac9
wxyzeero <wxyzeero@gmail.com>
2024-10-10 20:26:31 -0300

    fix: tag annotation and message

--- tag/create/create-tag.sh
+++ tag/create/create-tag.sh
@@ -6,10 +6,10 @@ COMMIT_LIST=$3
 COMMIT_LIST_SEP=$4
 COMMIT_MESSAGE=$(printf "${TAG_MESSAGE}" "${NEW_VERSION}" "${COMMIT_LIST//$COMMIT_LIST_SEP//$'\n'}")
 
-echo "COMMIT_MESSAGE: ${NEW_VERSION}" >&2
-
 git push origin :v${NEW_VERSION}
 git tag -d v${NEW_VERSION}
 
-git tag -a -m "${MESSAGE}" "v${NEW_VERSION}"
+echo "COMMIT_MESSAGE: ${COMMIT_MESSAGE}" >&2
+
+git tag -a -m "${COMMIT_MESSAGE}" "v${NEW_VERSION}"
 git push origin tag "v${NEW_VERSION}"
