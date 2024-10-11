7a8b19668db41df4ddf9862c6cf6f61f978b92ff
wxyzeero <wxyzeero@gmail.com>
2024-10-10 21:17:24 -0300

    fix: get package latest version from api

--- version/current/get-current-version.sh
+++ version/current/get-current-version.sh
@@ -2,6 +2,7 @@
 
 VERSION_FROM=$1
 DEFAULT_VERSION=$2
+PACKAGE_NAME=$3
 
 echo "DEFAULT_VERSION: ${DEFAULT_VERSION}" >&2
 
@@ -9,16 +10,22 @@ MATCH_PATTERN="*$(git tag | grep -E "^(releases?\/|v)?[0-9]+\.[0-9]+\.[0-9]+$" |
 
 if [[ $VERSION_FROM == "git" ]]; then
 
+  ONLINE_VERSION=""
   LOCAL_VERSION=$(git describe --tags --abbrev=0 --match="${MATCH_PATTERN}")
-  ONLINE_VERSION=$(npm info get version --silent)
   
+  if [[ $PACKAGE_NAME != "" ]]; then 
+    ONLINE_VERSION=$(curl https://registry.npmjs.org/${PACKAGE_NAME} | jq '."dist-tags".latest' -r)
+  else 
+    ONLINE_VERSION=$(npm info get version --silent)
+  fi
+
   if [[ $ONLINE_VERSION != "" ]]; then
     echo "CURRENT_VERSION from ONLINE: ${ONLINE_VERSION}" >&2
     DEFAULT_VERSION=$ONLINE_VERSION
   else 
-    if [[ $CURRENT_VERSION != "" ]]; then
-      echo "CURRENT_VERSION from LOCAL: ${CURRENT_VERSION}" >&2
-      DEFAULT_VERSION=$(echo "$CURRENT_VERSION" | cut -d'v' -f 2)
+    if [[ $LOCAL_VERSION != "" ]]; then
+      echo "CURRENT_VERSION from LOCAL: ${LOCAL_VERSION}" >&2
+      DEFAULT_VERSION=$(echo "$LOCAL_VERSION" | cut -d'v' -f 2)
     else
       echo "CURRENT_VERSION: Not Found" >&2
     fi
